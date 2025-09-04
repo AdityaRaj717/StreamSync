7677456a15b0a76f048bfafda9699421797e70a8
AdityaRaj717 <rajaditya81795@gmail.com>
2025-09-04 15:44:01 +0530

    Added user's video feed to the webpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import { io } from "socket.io-client";
-import adapter from "webrtc-adapter";
+// import adapter from "webrtc-adapter";
 
 function App() {
+  const [stream, setStream] = useState(null);
+  const videoRef = useRef(null);
+
   useEffect(() => {
     const socket = io("https://localhost:3000/");
     socket.on("connect", () => {
@@ -10,17 +13,37 @@ function App() {
     });
   }, []);
 
-  function enableVideoAudio() {
-    const stream = navigator.mediaDevices.getDisplayMedia({
+  useEffect(() => {
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  async function enableVideoAudio() {
+    const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true,
     });
+    setStream(stream);
   }
 
   return (
-    <>
-      <button onClick={enableVideoAudio}>Enable Stream</button>
-    </>
+    <div className="h-[90vh] flex flex-col gap-5 items-center justify-center">
+      <h1 className="border-2 p-5">StreamSync</h1>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        muted
+        className="w-96 h-96 border-2"
+      />
+      <button
+        onClick={enableVideoAudio}
+        className="border-2 p-2 w-2xs cursor-pointer"
+      >
+        Start Camera
+      </button>
+    </div>
   );
 }
 
